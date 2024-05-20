df262a3dc2ba439ee30699fffe57222a9b5756ba
Luis Orozco <luisoroz981@gmail.com>
2024-05-20 16:15:34 -0600

    Conociendo a Juan

---
+++ retos_sesion_03/ejercicio_02.py
@@ -0,0 +1,8 @@
+print("Hola Carlos, como estas")
+print("Hola Juan, bien y tu")
+print("Cual es tu comoida favorita Carlos")
+print("Pollo frito, y la tuya")
+print("mole con pollo")
+print("De donde eres Juan")
+print("Mexico y tu Juan")
+print("Soy de Colombia")
