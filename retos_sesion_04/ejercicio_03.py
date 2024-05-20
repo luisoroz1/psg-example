aad2a3e53b1fdb07af9ca1257649898ca298ac97
Luis Orozco <luisoroz981@gmail.com>
2024-05-20 17:51:20 -0600

    reglas del ajedrez

---
+++ retos_sesion_04/ejercicio_03.py
@@ -0,0 +1,34 @@
+Reglas de ajedrez
+
+"""Las normas que regulan el juego entre dos jugadores que se enfrentan con piezas blancas
+y negras sobre un tablero de 64 casillas. Las reglas básicas de juego son 12:"""
+
+"""
+1.  El juego es por turnos entre dos contrincantes, el jugador con las piezas blancas mueve
+    primero.
+2.  La colocación inicial de las piezas es especular para ambos bandos. El tablero debe 
+    estar colocado con la casilla oscura en la esquina izquierda, los peones en la segunda 
+    fila, la reina y el rey en las casillas centrales de la primera fila, los alfiles, 
+    los caballos y las torres en ambos costados.
+3.  El Rey puede moverse en todas las direcciones adyacentes.
+4.  La Torre se mueve en línea recta horizontal o vertical en cualquier número de casillas
+    libres hasta llegar al final del tablero o ser bloqueada por otra pieza.
+5.  El Alfil se mueve en línea recta diagonal, puede moverse tantas casillas como desee
+    hasta llegar al final del tablero o ser bloqueado por otra pieza.
+6.  La Reina es la pieza más poderosa, puede moverse en cualquier dirección horizontal, 
+    vertical o diagonal en cualquier número de casillas libres.
+7.  El Caballo es la única pieza que puede saltar sobre otras piezas, se mueve en forma de
+    «L» (dos casillas en dirección horizontal o vertical y una en ángulo recto).
+8.  El Peón se mueve solo hacia adelante, a excepción de su primera jugada, en la cual puede
+    avanzar dos casillas. El Peón captura en diagonal.
+9.  La Promoción del Peón es cuando este logra alcanzar el final del tablero y se convierte
+    en la pieza que se elija entre Reina, Torre, Alfil o Caballo.
+10. El Enroque se realiza con el Rey y una Torre, requiere que ambas piezas no hayan sido
+    movidas previamente, no haya piezas entre ellas y que el Rey no esté en jaque. 
+    Hay dos tipos de enroque: corto y largo.
+11. El Jaque es una situación en la que el Rey está bajo amenaza de ser capturado por una 
+    pieza del oponente, pero tiene la posibilidad de salir de esta situación. El Rey debe 
+    salir de esta situación en su próximo movimiento.
+12. El objetivo del juego es acorralar al rey del oponente hasta lograr que no tenga 
+    escapatorias y lo que se denomina Jaque Mate, lo que termina la partida.
+"""
