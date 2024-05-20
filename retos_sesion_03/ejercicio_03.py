b86ca0229b534f9bed13ad3453d9e5430ef6f9cb
Luis Orozco <luisoroz981@gmail.com>
2024-05-20 16:26:08 -0600

    Como barrer la habitacion

---
+++ retos_sesion_03/ejercicio_03.py
@@ -0,0 +1,10 @@
+# Como barrer una habitación
+"""
+1.  Sujeta la escoba con ambas manos, colocando una mano arriba y la otra cerca del centro, 
+    moviéndola desde lejos del cuerpo hacia adentro
+2.  Comienza en los bordes de la habitación y trabaja hacia el centro. Aleja los residuos de los 
+    zócalos y sácalos a un espacio abierto, donde podrás apilarlos para recogerlos con facilidad más adelante.
+3.  Una vez que hayas barrido los bordes de la habitación, enfócate en recoger los residuos acumulados 
+    con la pala. Luego, levanta la pala con cuidado, vacía el contenido en un contenedor de basura 
+    cercano y ¡listo.
+"""
